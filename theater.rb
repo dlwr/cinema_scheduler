bb8b681820fd21832ed473dee99f1639a618ac70
Meigaza Taro <tokyo.meigaza@gmail.com>
2013-02-14 16:24:07 +0900

    theater

---
+++ theater.rb
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+$theater = Hash.new
+$theater["K's cinema"] = {:path => "th487", :cal_id => "ama6sg1mdi52plkn0296cln49g@group.calendar.google.com"}
+$theater["TOHOシネマズ スカラ座・みゆき座"] = {:path => "th643", :cal_id => "hs2h7hvljhifoketaksorgajgs@group.calendar.google.com"}
+$theater["TOHOシネマズ 府中"] = {:path => "th426", :cal_id => "2lfbutpbqcp8nsob18kh4vpb4g@group.calendar.google.com"}
+$theater["TOHOシネマズ 六本木"] = {:path => "th107", :cal_id => "bnrfumotdlikrb5acu10r967hc@group.calendar.google.com"}
+$theater["アップリンク"] = {:path => "th327", :cal_id => "g5u0br3tr49m9ebc0qq115dte8@group.calendar.google.com"}
+$theater["オーディトリウム渋谷"] = {:path => "th663", :cal_id => "vb1ashasn2rrcghkph94ccvlgk@group.calendar.google.com"}
+$theater["キネカ大森"] = {:path => "th197", :cal_id => "j8uo3jue9gpfnk4ac5l256ag7g@group.calendar.google.com"}
+$theater["シネマート新宿"] = {:path => "th59", :cal_id => "9pgi7ctbcfut8tmff58nbtvsg0@group.calendar.google.com"}
+$theater["シネマート六本木"] = {:path => "th365", :cal_id => "smc95ouur1efq0ptr5ijev0g4o@group.calendar.google.com"}
+$theater["シネマヴェーラ渋谷"] = {:path => "th170", :cal_id => "7vaqlr1585bidb5nh4sbr98qro@group.calendar.google.com"}
+$theater["シネマシティ／CINEMA・TWO"] = {:path => "th257", :cal_id => "5air2ob0pkg9v4sldbd0ccqs64@group.calendar.google.com"}
+$theater["シネマブルースタジオ"] = {:path => "th180", :cal_id => "f7f8i9bej18gv9nf2uhm7q5dms@group.calendar.google.com"}
+$theater["テアトル新宿"] = {:path => "th378", :cal_id => "f873l92rnjo4tvc48kt3s6c2r8@group.calendar.google.com"}
+$theater["ヒューマントラストシネマ渋谷"] = {:path => "th56", :cal_id => "ms97jlns9ckfonbqvmu9ivf2u8@group.calendar.google.com"}
+$theater["ポレポレ東中野"] = {:path => "th567", :cal_id => "d34p8evb3hjskda856rng3srh8@group.calendar.google.com"}
+$theater["ユーロスペース"] = {:path => "th319", :cal_id => "r89mome8pqaadtk545qks91jkc@group.calendar.google.com"}
+$theater["ユナイテッド・シネマ豊洲"] = {:path => "th364", :cal_id => "li13787c3olenj7ip2jdsbfsa0@group.calendar.google.com"}
+$theater["ラピュタ阿佐ヶ谷"] = {:path => "th194", :cal_id => "ph9ftr4reofoa1ln2cm09veii0@group.calendar.google.com"}
+$theater["下高井戸シネマ"] = {:path => "th322", :cal_id => "5pnoosebe95bhujmvjlrfbre7c@group.calendar.google.com"}
+$theater["角川シネマ新宿"] = {:path => "th171", :cal_id => "2ibf9b36vco6bj91i49n4ge62g@group.calendar.google.com"}
+$theater["楽天地シネマズ錦糸町"] = {:path => "th570", :cal_id => "e3lqrb4j67mh4fl6q0tqlhtqbc@group.calendar.google.com"}
+$theater["吉祥寺バウスシアター"] = {:path => "th440", :cal_id => "5qp9rpm39egj8gp9657e8obu6k@group.calendar.google.com"}
+$theater["銀座シネパトス"] = {:path => "th122", :cal_id => "puutou9qc9icj02kdgrk0icrc8@group.calendar.google.com"}
+$theater["銀座テアトルシネマ"] = {:path => "th507", :cal_id => "qikrm9pjtuv67dssdh1qa2dums@group.calendar.google.com"}
+$theater["三軒茶屋シネマ"] = {:path => "th71", :cal_id => "734v6lj3ps437eleutm451bp28@group.calendar.google.com"}
+$theater["三軒茶屋中央劇場"] = {:path => "th569", :cal_id => "nlsi4kr204g4kipe2ottl8u4l0@group.calendar.google.com"}
+$theater["新橋文化劇場"] = {:path => "th512", :cal_id => "h7njkt6snu8s92aoup3lbhvjnc@group.calendar.google.com"}
+$theater["新宿バルト9"] = {:path => "th309", :cal_id => "cjajq80c4e5k3vi71ql1vn12lo@group.calendar.google.com"}
+$theater["新宿ピカデリー"] = {:path => "th432", :cal_id => "d614a44krt942k6baikillj9s0@group.calendar.google.com"}
+$theater["新宿武蔵野館"] = {:path => "th2", :cal_id => "728gmubpd30mk5grhhi5auf904@group.calendar.google.com"}
+$theater["新文芸坐"] = {:path => "th13", :cal_id => "f6gap54vsjt6464dojmhrh0kao@group.calendar.google.com"}
+$theater["神保町シアター"] = {:path => "th368", :cal_id => "c7si61dvn3v3oos9a7s7s5bl0c@group.calendar.google.com"}
+$theater["早稲田松竹"] = {:path => "th566", :cal_id => "fhifsq8de2d6miov8ol09g62p8@group.calendar.google.com"}
+$theater["東京国立近代美術館フィルムセンター"] = {:path => "th414", :cal_id => "gs1bfal2gievni8kitid5licdc@group.calendar.google.com"}
+$theater["東劇"] = {:path => "th317", :cal_id => "si6dco6fivl3e6q18anoa5aps4@group.calendar.google.com"}
+$theater["飯田橋ギンレイホール"] = {:path => "th321", :cal_id => "mfhk5pi91dq6sgamssio8jt1lg@group.calendar.google.com"}
+$theater["品川プリンスシネマ"] = {:path => "th131", :cal_id => "6urqb0ropok0ppgcq8k0rg9a1o@group.calendar.google.com"}
+$theater["目黒シネマ"] = {:path => "th68", :cal_id => "3j0ahbi86c1djcm4km0hc8960c@group.calendar.google.com"}
+
